b11f2c9689cfd7cb0088abe2540522f79d2aede2
andychow326 <andychchow326@gmail.com>
2022-08-25 15:58:07 +0800

    Add login button to Home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import authgear from "@authgear/web";
 
-<template><div>Hello World</div></template>
+const startLogin = () => {
+  authgear
+    .startAuthentication({
+      redirectURI: "http://localhost:4000/auth-redirect",
+      prompt: "login",
+    })
+    .then(
+      () => {
+        // started authorization, user should be redirected to Authgear
+      },
+      (err) => {
+        // failed to start authorization
+        console.error(err);
+      }
+    );
+};
+</script>
+
+<template>
+  <h1>Home Page</h1>
+  <button @click="startLogin">Login</button>
+</template>
